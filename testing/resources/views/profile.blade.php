9cdb838bee11cd65dc54be7f0cca99bcc6a55403
Rovalina Andini <rovalinaandini@gmail.com>
2025-09-30 13:09:36 +0700

    modul 2

---
+++ testing/resources/views/profile.blade.php
@@ -0,0 +1,3 @@
+<h1>Profile Page</h1>
+<p>Nama : Rovalina Andini</p>
+<p>Jurusan : D4 SIstem Informasi Kota Cerdas</p>
--- testing/routes/web.php
+++ testing/routes/web.php
@@ -1,18 +1,31 @@
 <?php
 
-// use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ('Selamat datang di halaman utama');
+    return ('selamat datang di halaman utama!');
 });
+
+Route::get('/hello', function () {
+    return 'Hello, Rovalina Andini';
+});
+
+Route::get('/user/{andini}', function ($name){
+    return "Nama saya $name";
+});
+
+Route::get('/greet/{name?}', function ($name = 'andini') {
+    return "Halo, $name";
+});
+
+Route::get('/profile',function () {
+    return view('profile');
+});
+
+Route::get('about', function() {
+    return view('about', ['name' => 'Rovalina Andini']);
+});
+
+Route::get('/home', function () {
+    return 'Halo, Ini adalah halaman Home';
+}) ->name('home.page');
