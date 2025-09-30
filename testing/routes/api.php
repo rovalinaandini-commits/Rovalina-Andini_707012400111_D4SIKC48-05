dc17698f5440f1e52dcf8b6c11b31f2dc65b9725
Rovalina Andini <rovalinaandini@gmail.com>
2025-09-30 11:36:47 +0700

    first commit

---
+++ testing/routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::get('/produk', function (){
+    return response()->json([
+        ['id' => 1, 'nama' => 'Laptop Gaming'],
+        ['id' => 2, 'nama' => 'Mechanical Keyboard'],
+    ]);
+});
+
+
+
